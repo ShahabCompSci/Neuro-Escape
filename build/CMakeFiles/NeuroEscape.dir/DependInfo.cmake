d5580fe8617bad329751d567195f8090af7717db
Shahab <shahabjamali04@gmail.com>
2025-03-27 18:16:29 +0000

    Implemented Player class with keyboard-controlled rectangle: - Created Player class for movement - Added input handling with WASD keys - Integrated player update and render in main game loop

---
+++ build/CMakeFiles/NeuroEscape.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Personal Github Repo/Neuro-Escape/src/Game.cpp" "CMakeFiles/NeuroEscape.dir/src/Game.cpp.obj" "gcc" "CMakeFiles/NeuroEscape.dir/src/Game.cpp.obj.d"
+  "C:/Personal Github Repo/Neuro-Escape/src/Player.cpp" "CMakeFiles/NeuroEscape.dir/src/Player.cpp.obj" "gcc" "CMakeFiles/NeuroEscape.dir/src/Player.cpp.obj.d"
+  "C:/Personal Github Repo/Neuro-Escape/src/main.cpp" "CMakeFiles/NeuroEscape.dir/src/main.cpp.obj" "gcc" "CMakeFiles/NeuroEscape.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/NeuroEscape.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NeuroEscape.dir/src/Game.cpp.obj"
+  "CMakeFiles/NeuroEscape.dir/src/Game.cpp.obj.d"
+  "CMakeFiles/NeuroEscape.dir/src/Player.cpp.obj"
+  "CMakeFiles/NeuroEscape.dir/src/Player.cpp.obj.d"
+  "CMakeFiles/NeuroEscape.dir/src/main.cpp.obj"
+  "CMakeFiles/NeuroEscape.dir/src/main.cpp.obj.d"
+  "NeuroEscape.exe"
+  "NeuroEscape.exe.manifest"
+  "NeuroEscape.pdb"
+  "libNeuroEscape.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NeuroEscape.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
